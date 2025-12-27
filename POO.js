403d0c8f73ac68426708dd76d63f7dc183809c59
Felipe <juanfecalderon456@gmail.com>
2025-12-26 22:08:02 -0500

    Primer commit: agregando POO.js

---
+++ POO.js
@@ -0,0 +1,7 @@
+const prompt=require("prompt-sync")()
+const yo={
+    name:prompt("porfavor ingrese su nombre : "),
+    edad:prompt("ingrese su edad : ") ,
+    Id:prompt("ingrese su identificacion :  "),
+} 
+console.log(yo)
