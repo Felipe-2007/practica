7bf54cb9affbe7b4fe79a49d7d44d60eb59b9252
Felipe <juanfecalderon456@gmail.com>
2025-12-26 22:15:11 -0500

    e

--- POO.js
+++ POO.js
@@ -1,7 +1,8 @@
 const prompt=require("prompt-sync")()
-const yo={
+const yoo={
     name:prompt("porfavor ingrese su nombre : "),
     edad:prompt("ingrese su edad : ") ,
     Id:prompt("ingrese su identificacion :  "),
 } 
-console.log(yo)
+console.log(yoo)
+console.log("8")
